1f6e0d7f62e72d9378394117a28d4998dbe198bc
Zuhair Hossain <szhzuhair@gmail.com>
2020-06-10 06:32:17 +0000

    added range

--- main.cs
+++ main.cs
@@ -4,9 +4,18 @@ namespace MyCalculator
 {
     class Program
     {
+        private const double _MIN_VALUE_Double = -1000;
+        private const double _MAX_VALUE_Double = 1000;
+ 
         static void Main(string[] args)
         {
+            double firstDouble = 0;
+            double secondDouble = 0;
+            double totalDouble = 0;
+            bool OKBool = true;
+ 
             Console.WriteLine("Welcome to My Calculator.");
+ 
             Console.Write("Press ENTER key to end program.");
             Console.ReadLine();
         }
